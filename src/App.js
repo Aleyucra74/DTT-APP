cc1103dc76066cc5e88fbbbcd48dbf3bb28edd67
aleyucra74 <alexandre.yucra@outlook.com>
2021-12-01 08:47:25 -0300

    some changes - front

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+
+import DttImg from './img/BlackBackgorundDeloitte.png';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header className="bgc-black flex flex-center min-h shadow-btm">
+        <img className="h-auto" src="https://web.deloitte.com.br/sai/apontamentohorasweb/Images/BlackBackgorundDeloitte.png" alt="Profile" />
       </header>
+
+      {/* section main */}
+      <main className="container flex justify-between flex-rol">
+        <form className="flex margin-top">
+          <div className="w-75 flex flex-rol margin">
+            <input 
+              className="border rounded pl-inside text-size"
+              style={{marginBottom: "10px"}}
+              placeholder="Some text"
+              type="text" 
+            />
+            <input 
+              className="border roudend"
+              type="file" 
+            />
+          </div>
+          <div className="w-25">
+            <input 
+              className="border-none bgc-blue rounded text-white pl focus"
+              value="Enviar" 
+              type="button" 
+            />
+          </div>
+        </form>
+
+        {/* footer */}
+        <footer>
+          &copy; Deloitte - 2021
+        </footer>
+      </main>
     </div>
   );
 }
