dda809cbdd1d8b1bac656f32634a975cabba3982
aleyucra74 <alexandre.yucra@outlook.com>
2021-12-12 18:24:24 -0300

    add some stuff - render  data nw

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import axios from 'axios';
 
@@ -6,8 +6,9 @@ import axios from 'axios';
 
 function App() {
   const [file, setFile] = useState();
-	const [isFilePicked, setIsFilePicked] = useState(false);
-  const [reposta,setResposta] = useState();
+  const [isFilePicked, setIsFilePicked] = useState(false);
+  const [nota, setNota] = useState({});
+  let x = "";
 
   async function handleSubmit(event) {
     event.preventDefault()
@@ -15,23 +16,34 @@ function App() {
     formData.append("file", file);
     try {
       const response = await axios({
-        method: "get",
-        url: "http://6815-35-233-128-182.ngrok.io/",
+        method: "post",
+        url: "http://8367-35-221-3-11.ngrok.io/",
         data: formData,
         headers: { "Content-Type": "multipart/form-data" },
       }).then((resposta) => {
         console.log(resposta);
-        // setResposta(resposta)
+        x = resposta.data;
       })
-    } catch(error) {
+    } catch (error) {
       console.log(error)
     }
+    console.log(x);
   }
 
   const changeHandler = (event) => {
-		setFile(event.target.files[0]);
-		setIsFilePicked(true);
-	};
+    setFile(event.target.files[0]);
+    setIsFilePicked(true);
+  };
+
+  function renderPrediction() {
+    if(x.predict == 0){
+      return <h3>Macae</h3>
+    } else if (x.predict == 1){
+      return <h3>Rio de janeiro</h3>
+    } else if(x.predict ==2){
+      return <h3>São Paulo</h3>
+    }
+  }
 
   return (
     <div>
@@ -43,20 +55,20 @@ function App() {
       <main className="container flex justify-between flex-rol">
         <form className="flex margin-top" onSubmit={(e) => handleSubmit(e)} encType="multipart/form-data">
           <div className="w-75 flex flex-rol margin">
-          {isFilePicked ? (
-				<div>
-					<p>Filename: {file.name}</p>
-					<p>Filetype: {file.type}</p>
-					<p>Size in bytes: {file.size}</p>
-					<p>
-						lastModifiedDate:{' '}
-						{file.lastModifiedDate.toLocaleDateString()}
-					</p>
-				</div>
-			) : (
-				<p>Select a file to show details</p>
-			)}
-            <input 
+            {isFilePicked ? (
+              <div>
+                <p>Filename: {file.name}</p>
+                <p>Filetype: {file.type}</p>
+                <p>Size in bytes: {file.size}</p>
+                <p>
+                  lastModifiedDate:{' '}
+                  {file.lastModifiedDate.toLocaleDateString()}
+                </p>
+              </div>
+            ) : (
+              <p>Select a file to show details</p>
+            )}
+            <input
               className="border roudend"
               onChange={(e) => changeHandler(e)}
               type="file"
@@ -64,14 +76,16 @@ function App() {
             />
           </div>
           <div className="w-25">
-            <input 
+            <input
               className="border-none bgc-blue rounded text-white pl focus"
-              value="Enviar" 
+              value="Enviar"
               type="submit"
             />
           </div>
         </form>
-
+        <div>
+          {renderPrediction()}
+        </div>
         {/* footer */}
         <footer>
           &copy; Deloitte - 2021
